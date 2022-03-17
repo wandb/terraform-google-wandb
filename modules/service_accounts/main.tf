1517f9c10a3c75fbe8a18395755068647b9b3108
Venkata <venkata@wandb.com>
2022-03-17 16:11:27 -0700

    updates service account roles

--- modules/service_accounts/main.tf
+++ modules/service_accounts/main.tf
@@ -1,3 +1,5 @@
+data "google_client_config" "current" {}
+
 resource "random_id" "main" {
   # 30 bytes ensures that enough characters are generated to satisfy the service account ID requirements, regardless of
   # the prefix.
@@ -12,17 +14,45 @@ resource "google_service_account" "main" {
   description  = "Service Account used by Weights & Biases."
 }
 
+# Creates a managed service account key (credentials key)
 resource "google_service_account_key" "main" {
   service_account_id = google_service_account.main.name
 }
 
-resource "google_project_iam_binding" "token_creator_binding" {
-  project = "your-project-id" // project_id is null as part of the root main.tf
-  role    = [
-  "roles/iam.serviceAccountTokenCreator",
-  "roles/pubsub.admin",
-  "roles/composer.environmentAndStorageObjectAdmin",
-  "roles/cloudsql.client"
-] 
-  members = "serviceAccount:${google_service_account.sa-name.email}"
+locals {
+  sa_member  = "serviceAccount:${google_service_account.main.email}"
+  project_id = data.google_client_config.current.project
+}
+
+# Service Account Token Creator role allows principals to impersonate
+# service accounts to create OAuth 2.0 tokens which can be used to
+# authenticate with Google APIs 
+resource "google_project_iam_member" "token_creator" {
+  project = local.project_id
+  role    = "roles/iam.serviceAccountTokenCreator"
+  member  = local.sa_member
+}
+
+# PubSub Admin role provides service account members 
+# full access to topics and subscriptions
+resource "google_project_iam_member" "pubsub_admin" {
+  project = local.project_id
+  role    = "roles/pubsub.admin"
+  member  = local.sa_member
+}
+
+# Storage Object Admin role grants service account members
+# full control of objects in a storage bucket
+resource "google_project_iam_member" "storage_object_admin" {
+  project = local.project_id
+  role    = "roles/storage.objectAdmin"
+  member  = local.sa_member
+}
+
+# Cloud SQL Client role allows service account members
+# connectivity access to Cloud SQL instances
+resource "google_project_iam_member" "cloudsql_client" {
+  project = local.project_id
+  role    = "roles/cloudsql.client"
+  member  = local.sa_member
 }
