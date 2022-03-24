d4900ff72dae7918e1e8032941f57b1e5698bf92
Justin Brooks <jsbroks@gmail.com>
2022-03-24 23:45:40 +0000

    chore: remove certificate

--- modules/certificate/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ip_address" {
-  value = google_compute_global_address.default.address
-}
--- modules/certificate/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "namespace" {
-  type        = string
-  description = "String used for prefix resources."
-}
-
-variable "fqdn" {
-  type        = string
-  description = "The FQDN to the W&B application"
-}
