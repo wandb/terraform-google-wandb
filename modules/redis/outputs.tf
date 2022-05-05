3f08d7b044e97d3fc3ccef1f659aabb196763bb3
Elaina Hodgkin <ehodgkin9418@gmail.com>
2022-05-05 07:30:25 -0700

    feat: Use Redis (#17)
    * enable external redis
    
    * update version
    
    * terraform-docs: automated action
    
    * update cert path
    
    * terraform-docs: automated action
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/redis/outputs.tf
+++ modules/redis/outputs.tf
@@ -1,3 +1,12 @@
 output "connection_string" {
   value = "${google_redis_instance.default.host}:${google_redis_instance.default.port}"
 }
+
+output "ca_cert" {
+  value = "${google_redis_instance.default.server_ca_certs[0].cert}"
+}
+
+output "auth_string" {
+  value = "${google_redis_instance.default.auth_string}"
+}
+
