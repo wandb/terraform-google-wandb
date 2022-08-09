1a2f22a56b799a02327a9ef96af4065ff5ce3c18
Venkata <venkata@wandb.com>
2022-08-08 17:03:08 -0700

    fix: Fails without having IPv4 or IPv6 enabled

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -43,7 +43,7 @@ resource "google_sql_database_instance" "default" {
     }
 
     ip_configuration {
-      ipv4_enabled    = false
+      ipv4_enabled    = true
       private_network = var.network_connection.network
     }
 
