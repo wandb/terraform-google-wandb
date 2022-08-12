b8d27deea7557fb9ad220f5ae3438b5ded48c989
Justin Brooks <jsbroks@gmail.com>
2022-08-12 20:16:45 +0000

    fix: Add log writer permission

--- modules/service_accounts/main.tf
+++ modules/service_accounts/main.tf
@@ -30,12 +30,28 @@ resource "google_service_account_iam_member" "token_creator" {
   member             = local.sa_member
 }
 
+# For some reason we need this permission otherwise backend is throwing an error
+# hopfully this is a short term fix.
+resource "google_project_iam_member" "log_writer" {
+  project = local.project_id
+  role    = "roles/logging.logWriter"
+  member  = local.sa_member
+}
+
 # Cloud SQL Client role allows service account members connectivity access to
 # Cloud SQL instances
 resource "google_project_iam_member" "cloudsql_client" {
   project = local.project_id
   role    = "roles/cloudsql.client"
   member  = local.sa_member
+}
+
+# If the bucket already exists, grant this new service account permission
+resource "google_storage_bucket_iam_member" "object_admin" {
+  count  = var.bucket_name != "" ? 1 : 0
+  bucket = var.bucket_name
+  member = local.sa_member
+  role   = "roles/storage.objectAdmin"
 }
 
 # If the bucket already exists, grant this new service account permission
