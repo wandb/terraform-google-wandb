5c001fa58baea4b88e926fd0f0d400d2e8dcbd54
Flamarion Jorge <29267749+flamarion@users.noreply.github.com>
2023-03-01 16:58:09 +0100

    fix: Add back the default mysql version (#54)
    * fix: Add back the default mysql version
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -131,6 +131,7 @@ variable "ssl" {
 variable "database_version" {
   description = "Version for MySQL"
   type        = string
+  default     = "MYSQL_8_0_29"
 }
 
 variable "database_machine_type" {
