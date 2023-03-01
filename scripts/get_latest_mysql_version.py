0ee932723be5dcab25f0bf0322a858a64304149d
Justin Brooks <jsbroks@gmail.com>
2023-03-01 04:30:43 +0000

    chore: CI to check for MySQL version

---
+++ scripts/get_latest_mysql_version.py
@@ -0,0 +1,23 @@
+import requests
+import re
+import os
+from packaging import version
+
+
+URL: str = 'https://cloud.google.com/sql/docs/mysql/admin-api/rest/v1beta4/SqlDatabaseVersion'
+
+
+if __name__ == "__main__":
+    results = requests.get(URL, timeout=30).text
+
+    versions = re.findall(r'MYSQL\w*', results)
+    sem_ver = set(s.replace('MYSQL_', '').replace('_', '.') for s in versions)
+
+    latest_version = sorted(sem_ver, key=version.Version)
+    latest_version.reverse()
+
+    if os.getenv("GITHUB_ACTIONS") == "true":
+        print(f'::set-output name=LATEST_VERSION::{latest_version[0]}')
+    else:
+        print(latest_version[0])
+
---
+++ scripts/set_variable_default.py
@@ -0,0 +1,25 @@
+import re
+import sys
+
+
+def set_variable_default(variable: str, value: str, content: str) -> str:
+    """Parses variable and replaces default value"""
+    pat = '(variable\ \"' + variable + '\"\ {[\s\w\"=.#]+default\s*=\s\")[\w\s\d]*(\"[\s\w\"]*})'
+    return re.sub(
+        pat,
+        r'\1' + value + r'\2',
+        content
+    )
+
+
+if __name__ == '__main__':
+    var, val, *var_file = sys.argv[1:]
+    file_path = var_file or 'variables.tf'
+
+    print(f"Setting \"{var}\" to \"{val}\" in \"{file_path}\"")
+
+    with open(file_path, 'r+', encoding="utf8") as f:
+        txt = f.read()
+        result = set_variable_default(var, val, txt)
+        f.seek(0)
+        f.write(result)
