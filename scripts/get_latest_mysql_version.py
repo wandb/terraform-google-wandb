9e9de56afe180d6420eabeb0544493687ef1a7f3
Justin Brooks <jsbroks@gmail.com>
2023-03-01 04:58:59 +0000

    filter out invalid versions

--- scripts/get_latest_mysql_version.py
+++ scripts/get_latest_mysql_version.py
@@ -1,17 +1,25 @@
-import requests
-import re
 import os
+import re
+import requests
+
 from packaging import version
 
 
 URL: str = 'https://cloud.google.com/sql/docs/mysql/admin-api/rest/v1beta4/SqlDatabaseVersion'
 
 
+def is_version_compatible(v: str) -> bool:
+    return re.match(r'^MYSQL_(8|5)', v)
+
+
 if __name__ == "__main__":
     results = requests.get(URL, timeout=30).text
 
     versions = re.findall(r'MYSQL\w*', results)
-    sem_ver = set(s.replace('MYSQL_', '').replace('_', '.') for s in versions)
+    compatible_versions = filter(is_version_compatible, versions)
+    sem_ver = set(s.replace('MYSQL_', '').replace('_', '.') for s in compatible_versions)
+    
+    print(sem_ver)
 
     latest_version = sorted(sem_ver, key=version.Version)
     latest_version.reverse()
