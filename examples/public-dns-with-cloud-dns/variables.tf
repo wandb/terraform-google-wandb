60d412b4b9de77912a72a96d2fc5545e16664de5
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2023-03-22 18:43:55 -0500

    feat: Update example to support force_ssl

--- examples/public-dns-with-cloud-dns/variables.tf
+++ examples/public-dns-with-cloud-dns/variables.tf
@@ -56,6 +56,12 @@ variable "database_sort_buffer_size" {
   default     = 262144
 }
 
+variable "force_ssl" {
+  description = "Enforce SSL through the usage of the Cloud SQL Proxy (cloudsql://) in the DB connection string"
+  type        = bool
+  default     = false
+}
+
 variable "database_machine_type" {
   description = "Specifies the machine type to be allocated for the database"
   type        = string
