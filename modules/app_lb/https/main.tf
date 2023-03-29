b5788565b0483d661bd088722456e6d4011dffde
Zachary Blasczyk <77289967+wandb-zacharyblasczyk@users.noreply.github.com>
2023-03-29 15:21:06 -0500

    feat: Add SSL policy to LB

--- modules/app_lb/https/main.tf
+++ modules/app_lb/https/main.tf
@@ -10,21 +10,25 @@ resource "google_compute_managed_ssl_certificate" "default" {
 # Configure an HTTPS proxy with the Google-managed certificate and route it to
 # the URL map
 resource "google_compute_target_https_proxy" "default" {
-  name    = "${var.namespace}-https-proxy"
-  url_map = var.url_map.id
-  ssl_certificates = [
-    google_compute_managed_ssl_certificate.default.id
-  ]
+  name             = "${var.namespace}-https-proxy"
+  url_map          = var.url_map.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.default.id]
+  ssl_policy       = google_compute_ssl_policy.ssl-policy.id
 }
 
 # Configure a global forwarding rule to route the HTTPS traffic on the IP
 # address to the target HTTPS proxy:
 resource "google_compute_global_forwarding_rule" "default" {
-  name = "${var.namespace}-https"
-
+  name       = "${var.namespace}-https"
   target     = google_compute_target_https_proxy.default.id
   port_range = "443"
   ip_address = var.ip_address
+  labels     = var.labels
+}
 
-  labels = var.labels
+# SSL Policy to apply to Target Https Proxy
+resource "google_compute_ssl_policy" "ssl-policy" {
+  name            = "ssl-policy"
+  profile         = "MODERN"
+  min_tls_version = "TLS_1_2"
 }
