30d4243eb31c4b04ad295d5e63868b980228bd8e
George Scott <george.scott@wandb.com>
2023-09-18 18:44:08 -0500

    feat: Update google provider (#83)
    * update google provider
    
    * updapte docs

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.31"
+      version = "~> 4.82"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
