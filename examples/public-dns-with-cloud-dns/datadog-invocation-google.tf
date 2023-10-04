2452c7cad0749aff7a073e0612db6f18ba42c120
George Scott <george.scott@wandb.com>
2023-10-04 17:39:12 -0500

    moduled install

--- examples/public-dns-with-cloud-dns/datadog-invocation-google.tf
+++ examples/public-dns-with-cloud-dns/datadog-invocation-google.tf
@@ -1,7 +1,3 @@
-variable "cluster_name" {
-  nullable = true
-  type     = string
-}
 variable "dd_api_key" {
   nullable = true
   type     = string
@@ -15,20 +11,20 @@ variable "dd_site" {
   type     = string
 }
 
- variable "k8s_cluster_ca_certificate" {
-   nullable = false
-   type     = string
- }
+#  variable "k8s_cluster_ca_certificate" {
+#    nullable = false
+#    type     = string
+#  }
 
- variable "k8s_host" {
-   nullable = false
-   type     = string
- }
+#  variable "k8s_host" {
+#    nullable = false
+#    type     = string
+#  }
 
- variable "k8s_token" {
-   nullable = false
-   type     = string
- }
+#  variable "k8s_token" {
+#    nullable = false
+#    type     = string
+#  }
 
 data "google_container_cluster" "wandb" {
   name = module.wandb.cluster_self_link
@@ -38,7 +34,7 @@ data "google_container_cluster" "wandb" {
    source             = "git::https://github.com/wandb/terraform-wandb-modules.git//datadog?ref=working"
 
    cloud_provider_tag = "aws"
-   cluster_name       = data.gke.cluster_name
+   cluster_name       = module.wandb.cluster_name
    database_tag       = "managed"
    api_key            = var.dd_api_key
    app_key            = var.dd_app_key
