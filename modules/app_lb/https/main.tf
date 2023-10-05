c30cc5f2efa841b9b5880e33adabdfafe1047616
Flamarion Jorge <flamarion.jorge@wandb.com>
2023-10-05 15:13:59 +0200

    fix: Fix ssl certificate recreation

--- modules/app_lb/https/main.tf
+++ modules/app_lb/https/main.tf
@@ -1,9 +1,21 @@
+resource "random_pet" "cert" {
+  length = 2
+  keepers = {
+    fqdn = var.fqdn
+  }
+}
+
+
 # Create a managed SSL certificate that's issued and renewed by Google
 resource "google_compute_managed_ssl_certificate" "default" {
-  name = "${var.namespace}-cert"
+  name = "${var.namespace}-cert-${random_pet.cert.id}"
 
   managed {
     domains = [var.fqdn]
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
