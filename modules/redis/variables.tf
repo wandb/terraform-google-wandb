c5ec02738cb4a047c44e8e8b14cb85c0c9620034
venky-wandb <92930965+venky-wandb@users.noreply.github.com>
2023-12-15 13:04:26 -0800

    feat: Update redis reserved ip range and Add deletion protection to database (#92)
    * feat: Update redis reserved ip range
    
    * update
    
    * parameterize redis reserved ip range
    
    * Adds instance deletion protection on cloud sql
    
    * terraform-docs: automated action
    
    * fix fmt
    
    * update
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/redis/variables.tf
+++ modules/redis/variables.tf
@@ -18,3 +18,8 @@ variable "memory_size_gb" {
   description = "The amount of memory which will be allocated to the Redis instance; this value must be expressed in gibibytes."
   type        = number
 }
+
+variable "reserved_ip_range" {
+  description = "This defines the IP range for REDIS peering connection"
+  type        = string
+}
