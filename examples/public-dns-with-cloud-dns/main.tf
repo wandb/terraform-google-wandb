74deeb48db673e1e68f0e3980915fab3545b1be9
Aditya Choudhari <aditya.choudhari@wandb.com>
2024-02-05 11:53:56 -0800

    remove ingress variables.tf

--- examples/public-dns-with-cloud-dns/main.tf
+++ examples/public-dns-with-cloud-dns/main.tf
@@ -51,7 +51,6 @@ module "wandb" {
   database_machine_type     = var.database_machine_type
 
   disable_code_saving = var.disable_code_saving
-  use_new_ingress     = var.use_new_ingress
 }
 
 # You'll want to update your DNS with the provisioned IP address
