f0730834735510de9442a2267e06d65283987dd0
Aditya Choudhari <aditya.choudhari@wandb.com>
2024-03-06 13:05:58 -0800

    use latest

--- main.tf
+++ main.tf
@@ -243,8 +243,7 @@ module "wandb" {
     }
   }
 
-  operator_chart_version = "1.1.2"
-  controller_image_tag   = "1.10.1"
+  controller_image_tag = "1.10.1"
 
   # Added `depends_on` to ensure old infrastructure is provisioned first. This addresses a critical scheduling challenge
   # where the Datadog DaemonSet could fail to provision due to CPU constraints. Ensuring the old infrastructure has priority
