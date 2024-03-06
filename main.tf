ee34bcd6bd245c65ccf50a07eeed8e210348a3eb
Aditya Choudhari <aditya.choudhari@wandb.com>
2024-03-06 11:06:12 -0800

    fix: Add ingress name

--- main.tf
+++ main.tf
@@ -231,6 +231,7 @@ module "wandb" {
       }
 
       ingress = {
+        name = var.namespace
         annotations = {
           "kubernetes.io/ingress.class"                 = "gce"
           "kubernetes.io/ingress.global-static-ip-name" = module.app_lb.address_operator_name
