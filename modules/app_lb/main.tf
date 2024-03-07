d12222bbf2a298ffa867361dbc5191cb599241b8
Aditya Choudhari <aditya.choudhari@wandb.com>
2024-03-06 17:56:27 -0800

    remove url map var from https

--- modules/app_lb/main.tf
+++ modules/app_lb/main.tf
@@ -12,7 +12,6 @@ module "https" {
   source     = "./https"
   fqdn       = var.fqdn
   namespace  = var.namespace
-  url_map    = module.url_map.app
   ip_address = google_compute_global_address.default.address
 
   labels = var.labels
