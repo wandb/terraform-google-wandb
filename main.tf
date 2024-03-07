0a3e1c08f3afdb6d2286eddd626cc7706cad6799
Aditya Choudhari <aditya.choudhari@wandb.com>
2024-03-07 11:14:04 -0800

    update ingress name

--- main.tf
+++ main.tf
@@ -230,6 +230,7 @@ module "wandb" {
       }
 
       ingress = {
+        nameOverride = var.namespace
         annotations = {
           "kubernetes.io/ingress.class"                 = "gce"
           "kubernetes.io/ingress.global-static-ip-name" = module.app_lb.address_operator_name
@@ -243,8 +244,8 @@ module "wandb" {
     }
   }
 
-  operator_chart_version = "1.1.0"
-  controller_image_tag   = "1.10.1"
+  operator_chart_version = "1.1.2"
+  controller_image_tag   = "1.11.1"
 
   # Added `depends_on` to ensure old infrastructure is provisioned first. This addresses a critical scheduling challenge
   # where the Datadog DaemonSet could fail to provision due to CPU constraints. Ensuring the old infrastructure has priority
