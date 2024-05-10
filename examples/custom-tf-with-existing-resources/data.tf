ba80ebfe2610f3f51f2a4bc7aa5c307c282fafc5
amanpruthi <aman.pruthi@velotio.com>
2024-05-10 13:28:09 +0530

    added examples

---
+++ examples/custom-tf-with-existing-resources/data.tf
@@ -0,0 +1,18 @@
+data "google_client_config" "current" {}
+
+data "google_compute_network" "default" {
+  name = var.network
+}
+
+
+data "google_compute_subnetwork" "default" {
+  name   = var.subnetwork
+}
+
+data "google_container_cluster" "default" {
+  name     = var.cluster_name
+}
+
+data "google_redis_instance" "default" {
+  name = var.redis_cluster_name
+}
