761d0e0768714729e092e193f22cc57e2d81a820
Jeff Raubitschek <jeff@wandb.com>
2024-08-28 18:58:25 -0700

    add clickhose module

---
+++ modules/clickhouse/main.tf
@@ -0,0 +1,23 @@
+resource "google_compute_address" "psc_endpoint_ip" {
+  #  You can specify an IP address if needed.
+  #  address      = "10.148.0.2"
+  address_type = "INTERNAL"
+  name         = "clickhouse-cloud-psc-${var.clickhouse_region}"
+  purpose      = "GCE_ENDPOINT"
+  subnetwork   = var.subnetwork
+}
+
+resource "google_compute_forwarding_rule" "clickhouse_cloud_psc" {
+  ip_address            = google_compute_address.psc_endpoint_ip.self_link
+  name                  = "ch-cloud-${var.clickhouse_region}"
+  network               = var.network
+  ## region                = var.region
+  load_balancing_scheme = ""
+  # service attachment
+  target = "https://www.googleapis.com/compute/v1/${var.clickhouse_private_endpoint_service_name}"
+}
+
+output "psc_connection_id" {
+  value       = google_compute_forwarding_rule.clickhouse_cloud_psc.psc_connection_id
+  description = "Add GCP PSC Connection ID to allow list on instance level."
+}
---
+++ modules/clickhouse/variables.tf
@@ -0,0 +1,31 @@
+variable "network" {
+  description = "Google Compute Engine network to which the cluster is connected."
+  type        = object({ self_link = string })
+}
+
+variable "subnetwork" {
+  description = "Google Compute Engine subnetwork in which the cluster's instances are launched."
+  type        = object({ self_link = string })
+}
+
+variable "clickhouse_private_endpoint_service_name" {
+  type        = string
+  description = "ClickHouse private endpoint 'Service name' (ends in -clickhouse-cloud)."
+  default     = ""
+
+  validation {
+    condition = can(regex("-clickhouse-cloud$", var.clickhouse_private_endpoint_service_name))
+    error_message = "ClickHouse Service name must end in '-clickhouse-cloud'."
+  }
+}
+
+variable "clickhouse_region" {
+  type        = string
+  description = "ClickHouse region (us-east1, us-central1, etc)."
+  default     = ""
+
+  validation {
+    condition = length(var.clickhouse_region) > 0
+    error_message = "Clickhouse Region should always be set if the private endpoint service name is specified."
+  }
+}
