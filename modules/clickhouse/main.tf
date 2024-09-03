2a3b92ee601e13f3d04e809d7098b452d91dcc28
Jeff Raubitschek <jeff@wandb.com>
2024-09-03 12:16:28 -0700

    move region

--- modules/clickhouse/main.tf
+++ modules/clickhouse/main.tf
@@ -5,13 +5,13 @@ resource "google_compute_address" "psc_endpoint_ip" {
   name         = "clickhouse-cloud-psc-${var.clickhouse_region}"
   purpose      = "GCE_ENDPOINT"
   subnetwork   = var.subnetwork
+  region       = var.clickhouse_region
 }
 
 resource "google_compute_forwarding_rule" "clickhouse_cloud_psc" {
   ip_address            = google_compute_address.psc_endpoint_ip.self_link
   name                  = "ch-cloud-${var.clickhouse_region}"
   network               = var.network
-  region                = var.clickhouse_region
   load_balancing_scheme = ""
   # service attachment
   target = "https://www.googleapis.com/compute/v1/${var.clickhouse_private_endpoint_service_name}"
