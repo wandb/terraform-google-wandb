2a7cc59d1f87d7fe88e4aa6ff810e7087833c6e5
Jeff Raubitschek <jeff@wandb.com>
2024-09-11 10:19:08 -0700

    feat: add clickhouse private service connect

---
+++ modules/clickhouse/main.tf
@@ -0,0 +1,50 @@
+resource "google_compute_subnetwork" "psc_network" {
+  name = "${var.namespace}-subnet-clickhouse"
+
+  region                   = var.clickhouse_region
+  ip_cidr_range            = var.clickhouse_reserved_ip_range
+  private_ip_google_access = true
+  network                  = var.network
+}
+
+resource "google_compute_address" "psc_endpoint_ip" {
+  name         = "${var.namespace}-clickhouse-psc-ip"
+  address_type = "INTERNAL"
+  purpose      = "GCE_ENDPOINT"
+  subnetwork   = google_compute_subnetwork.psc_network.self_link
+  region       = var.clickhouse_region
+}
+
+resource "google_compute_forwarding_rule" "psc_forward_rule" {
+  name                    = "${var.namespace}-clickhouse-psc-forward"
+  ip_address              = google_compute_address.psc_endpoint_ip.self_link
+  network                 = var.network
+  region                  = var.clickhouse_region
+  load_balancing_scheme   = ""
+  allow_psc_global_access = true
+
+  target = "https://www.googleapis.com/compute/v1/${var.clickhouse_private_endpoint_service_name}"
+}
+
+resource "google_dns_managed_zone" "psc_dns_zone" {
+  name          = "${var.namespace}-clickhouse-dns-zone"
+  description   = "Private DNS zone for accessing ClickHouse Cloud using Private Service Connect"
+  dns_name      = "${var.clickhouse_region}.p.gcp.clickhouse.cloud."
+  force_destroy = true
+  visibility    = "private"
+
+  // associate private DNS zone with network
+  private_visibility_config {
+    networks {
+      network_url = var.network
+    }
+  }
+}
+
+resource "google_dns_record_set" "psc_dns_record" {
+  name         = "*.${var.clickhouse_region}.p.gcp.clickhouse.cloud."
+  managed_zone = google_dns_managed_zone.psc_dns_zone.name
+  type         = "A"
+  rrdatas      = [ google_compute_address.psc_endpoint_ip.address ]
+  ttl          = 3600
+}
