cec77e635f0d0e928c1945b021f8c2885f41e74b
Jeff Raubitschek <jeff@wandb.com>
2024-09-11 08:32:30 -0700

    more cleanups

--- modules/clickhouse/main.tf
+++ modules/clickhouse/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_subnetwork" "psc_network" {
-  name = "${var.namespace}-subnet-clickhouse-${var.clickhouse_region}"
+  name = "${var.namespace}-subnet-clickhouse"
 
   region                   = var.clickhouse_region
   ip_cidr_range            = var.clickhouse_reserved_ip_range
@@ -8,38 +8,29 @@ resource "google_compute_subnetwork" "psc_network" {
 }
 
 resource "google_compute_address" "psc_endpoint_ip" {
+  name         = "${var.namespace}-clickhouse-psc-ip"
   address_type = "INTERNAL"
-  name         = "clickhouse-cloud-psc-${var.clickhouse_region}"
   purpose      = "GCE_ENDPOINT"
   subnetwork   = google_compute_subnetwork.psc_network.self_link
   region       = var.clickhouse_region
 }
 
-resource "google_compute_forwarding_rule" "clickhouse_cloud_psc" {
-  ip_address            = google_compute_address.psc_endpoint_ip.self_link
-  name                  = "ch-cloud-${var.clickhouse_region}"
-  network                  = var.network.id
-  region       = var.clickhouse_region
-  load_balancing_scheme = ""
-  target = "https://www.googleapis.com/compute/v1/${var.clickhouse_private_endpoint_service_name}"
+resource "google_compute_forwarding_rule" "psc_forward_rule" {
+  name                    = "${var.namespace}-clickhouse-psc-forward"
+  ip_address              = google_compute_address.psc_endpoint_ip.self_link
+  network                 = var.network.id
+  region                  = var.clickhouse_region
+  load_balancing_scheme   = ""
   allow_psc_global_access = true
 
-  # comment why this was needed
-  depends_on = [
-    google_compute_address.psc_endpoint_ip
-  ]
+  target = "https://www.googleapis.com/compute/v1/${var.clickhouse_private_endpoint_service_name}"
 }
 
-output "psc_connection_id" {
-  value       = google_compute_forwarding_rule.clickhouse_cloud_psc.psc_connection_id
-  description = "Add GCP PSC Connection ID to allow list on instance level."
-}
-
-resource "google_dns_managed_zone" "clickhouse_cloud_private_service_connect" {
+resource "google_dns_managed_zone" "psc_dns_zone" {
+  name          = "${var.namespace}-clickhouse-dns-zone"
   description   = "Private DNS zone for accessing ClickHouse Cloud using Private Service Connect"
   dns_name      = "${var.clickhouse_region}.p.gcp.clickhouse.cloud."
   force_destroy = false
-  name          = "clickhouse-cloud-private-service-connect-${var.clickhouse_region}"
   visibility    = "private"
 
   // associate private DNS zone with network
@@ -50,9 +41,9 @@ resource "google_dns_managed_zone" "clickhouse_cloud_private_service_connect" {
   }
 }
 
-resource "google_dns_record_set" "psc-wildcard" {
-  managed_zone = google_dns_managed_zone.clickhouse_cloud_private_service_connect.name
+resource "google_dns_record_set" "psc_dns_record" {
   name         = "*.${var.clickhouse_region}.p.gcp.clickhouse.cloud."
+  managed_zone = google_dns_managed_zone.psc_dns_zone.name
   type         = "A"
   rrdatas      = [ google_compute_address.psc_endpoint_ip.address ]
   ttl          = 3600
