8eed48e7aca5e8a2f2d7bcd2a2f14289bca2fd94
Daniel Barnes <dabarnes2b@gmail.com>
2024-12-17 07:47:12 +0900

    fix ref

--- main.tf
+++ main.tf
@@ -287,7 +287,7 @@ module "wandb" {
         defaultBucket = {
           provider = "gcs"
           name     = module.storage.bucket_name
-          path     = module.storage.bucket_path
+          path     = ""
         }
 
         mysql = {
