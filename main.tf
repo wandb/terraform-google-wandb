0c98fb5e68b84404d4485da206c1be15f65eb923
Daniel Barnes <dabarnes2b@gmail.com>
2025-01-08 08:25:51 +0900

    fix: Pass IAM identity to console

--- main.tf
+++ main.tf
@@ -336,6 +336,12 @@ module "wandb" {
         ]
       }
 
+      console = {
+        extraEnv = {
+          "REFERENCE_IDENTITY" = module.service_accounts.service_account.email
+        }
+      }
+
       ingress = {
         create       = var.public_access # external ingress for public connection
         nameOverride = var.namespace
