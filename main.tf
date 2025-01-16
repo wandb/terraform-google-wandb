33a7f4f53e4939bf2bec0937efeb126afb0ac768
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2025-01-15 23:15:32 -0600

    fix: Adding missing service account

--- main.tf
+++ main.tf
@@ -33,6 +33,7 @@ locals {
     weave_trace            = "wandb-weave-trace"
     settings_migration_job = "wandb-settings-migration-job"
     wandb_api              = "wandb-api"
+    wandb_glue             = "wandb-glue"
   }
   gke_machine_type      = coalesce(var.gke_machine_type, local.deployment_size[var.size].node_instance)
   max_node_count        = coalesce(var.gke_max_node_count, local.deployment_size[var.size].max_node_count)
@@ -412,6 +413,26 @@ module "wandb" {
         serviceAccount = var.create_workload_identity ? {
           annotations = { "iam.gke.io/gcp-service-account" = module.service_accounts.sa_account_role }
           name        = local.k8s_sa_map.settings_migration_job
+          } : {
+          name        = null
+          annotations = {}
+        }
+      }
+
+      api = {
+        serviceAccount = var.create_workload_identity ? {
+          annotations = { "iam.gke.io/gcp-service-account" = module.service_accounts.sa_account_role }
+          name        = local.k8s_sa_map.wandb_api
+          } : {
+          name        = null
+          annotations = {}
+        }
+      }
+
+      glue = {
+        serviceAccount = var.create_workload_identity ? {
+          annotations = { "iam.gke.io/gcp-service-account" = module.service_accounts.sa_account_role }
+          name        = local.k8s_sa_map.wandb_glue
           } : {
           name        = null
           annotations = {}
