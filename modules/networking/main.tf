c15fa59cac3a746bd2a686db5d467d168e1d1087
Matthew Hooker <mwhooker@gmail.com>
2025-02-03 10:33:46 -0800

    fix: Use google-beta
    Use the google-beta provider in the
    `google_service_networking_connection` to make sure dependent resources
    are cleaned up properly.
    
    Fix described in https://github.com/hashicorp/terraform-provider-google/issues/16275#issuecomment-1825752152

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -32,6 +32,7 @@ resource "google_service_networking_connection" "default" {
   network                 = google_compute_network.vpc.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+  provider                = google-beta
 }
 
 resource "google_compute_global_address" "api_psc" {
