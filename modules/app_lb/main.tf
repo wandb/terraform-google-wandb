ff35a15174cdc79cd5d4ddfc3d2a3e182f94534e
Daniel Barnes <dabarnes2b@gmail.com>
2025-02-15 03:58:47 +0900

    fix: Chunk out allowed cidrs (#227)

--- modules/app_lb/main.tf
+++ modules/app_lb/main.tf
@@ -34,14 +34,22 @@ resource "google_compute_security_policy_rule" "allow_internal" {
   action = "allow"
 }
 
+locals {
+  cidrs_chunked = [
+    for i in range(0, ceil(length(var.allowed_inbound_cidrs) / 10)) :
+    slice(var.allowed_inbound_cidrs, i * 10, min((i + 1) * 10, length(var.allowed_inbound_cidrs)))
+  ]
+}
+
 resource "google_compute_security_policy_rule" "allow_cidrs" {
+  for_each        = { for idx, cidrs in local.cidrs_chunked : idx => cidrs }
   security_policy = google_compute_security_policy.default.name
-  description     = "Allowed CIDRs"
-  priority        = 1001
+  description     = "Allowed CIDRs chunk ${each.key + 1}"
+  priority        = 1001 + each.key
   match {
     versioned_expr = "SRC_IPS_V1"
     config {
-      src_ip_ranges = var.allowed_inbound_cidrs
+      src_ip_ranges = each.value
     }
   }
   action = "allow"
