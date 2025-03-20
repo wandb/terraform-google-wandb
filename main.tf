c656e0b49b3fd7c97b32b6ce9d069b973a173ba7
Jonathan Meeks <jonathanmeeks@gmail.com>
2025-03-20 16:27:53 -0500

    INFRA-673 apply cache_size tag to AKS cluster

--- main.tf
+++ main.tf
@@ -136,7 +136,7 @@ module "app_gke" {
   depends_on                 = [module.project_factory_project_services]
   max_node_count             = local.max_node_count
   min_node_count             = local.min_node_count
-  labels                     = var.labels
+  labels                     = merge(var.labels, {cache_size = var.cache_size})
   enable_private_gke_nodes   = var.enable_private_gke_nodes
 }
 
