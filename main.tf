c477bfd3e36702de4e3f2a814182aedf8d171441
Daniel Barnes <dabarnes2b@gmail.com>
2025-03-29 04:57:43 +0900

    chore: Passthrough size

--- main.tf
+++ main.tf
@@ -330,6 +330,7 @@ module "wandb" {
   spec = {
     values = {
       global = {
+        size          = var.size
         pod           = { labels = { workload_hash : local.workload_hash } }
         host          = local.url
         license       = var.license
