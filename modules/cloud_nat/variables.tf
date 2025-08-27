6ec252b84005cb41ce484e1555a8e3abd301af64
Jonathan Meeks <jonathan.meeks@wandb.com>
2025-08-27 16:23:18 -0500

    fix: switch NLB NAT to dynamic port allocation

--- modules/cloud_nat/variables.tf
+++ modules/cloud_nat/variables.tf
@@ -12,6 +12,24 @@ variable "proxy_nat" {
   type        = bool
 }
 
+variable "proxy_nat_min_ports_per_vm" {
+  description = "Minimum number of ports to allocate per VM for the Load Balancer Proxy NAT"
+  type        = number
+  default     = 64
+}
+
+variable "proxy_nat_max_ports_per_vm" {
+  description = "Maximum number of ports to allocate per VM for the Load Balancer Proxy NAT"
+  type        = number
+  default     = 65536
+}
+
+variable "proxy_nat_enable_dynamic_port_allocation" {
+  description = "Enable dynamic port allocation for the Load Balancer Proxy NAT"
+  type        = bool
+  default     = true
+}
+
 variable "vpc_nat" {
   description = "Enable NAT for the VPC"
   type        = bool
